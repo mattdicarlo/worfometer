05c3869d87dca29462cdad6d4801fdec95f0f11d
Matt DiCarlo <dicarlo.matt@gmail.com>
2021-09-23 20:57:10 -0500

    Mock odometer option

--- odometer.py
+++ odometer.py
@@ -3,36 +3,63 @@
 from datetime import datetime, timedelta
 from typing import Tuple
 from util import utc_now
-import board        # type: ignore
-import digitalio    # type: ignore
 
 
 class Odometer:
     def __init__(self):
-        self.reed_switch = digitalio.DigitalInOut(board.D16)
-        self.reed_switch.direction = digitalio.Direction.INPUT
-        self.reed_switch.pull = digitalio.Pull.UP
+        import board        # type: ignore
+        import digitalio    # type: ignore
 
-        self.OPEN_STATE = True
-        self.DEBOUNCE = timedelta(milliseconds=100)
+        self._reed_switch = digitalio.DigitalInOut(board.D16)
+        self._reed_switch.direction = digitalio.Direction.INPUT
+        self._reed_switch.pull = digitalio.Pull.UP
 
-        self.previous_time = utc_now()
-        self.previous_state = self.OPEN_STATE
+        self._OPEN_STATE = True
+        self._DEBOUNCE = timedelta(milliseconds=100)
+
+        self._previous_time = utc_now()
+        self._previous_state = self.OPEN_STATE
+
         self.total_rotations = 0
 
     def check_rotations(self) -> Tuple[int, datetime]:
         current_time = utc_now()
-        delta = current_time - self.previous_time
-        if delta < self.DEBOUNCE:
+        delta = current_time - self._previous_time
+        if delta < self._DEBOUNCE:
             return (0, current_time)
 
-        current_state = self.reed_switch.value
-        if current_state != self.previous_state:
-            self.previous_time = current_time
-            self.previous_state = current_state
-            if current_state != self.OPEN_STATE:
+        current_state = self._reed_switch.value
+        if current_state != self._previous_state:
+            self._previous_time = current_time
+            self._previous_state = current_state
+            if current_state != self._OPEN_STATE:
                 self.total_rotations += 1
                 print(f'count: {self.total_rotations}\tdelta: {delta.total_seconds() * 1000}ms')
                 return (1, current_time)
+
+        return (0, current_time)
+
+
+class MockOdometer:
+    def __init__(self):
+        self._previous_time = utc_now()
+
+        self._DEBOUNCE = timedelta(milliseconds=100)
+
+        self.total_rotations = 0
+
+    def check_rotations(self) -> Tuple[int, datetime]:
+        import random
+        current_time = utc_now()
+        delta = current_time - self._previous_time
+
+        if delta < self._DEBOUNCE:
+            return (0, current_time)
+
+        if random.choices([True, False], weights=[1, 1000], k=1)[0] is True:
+            self._previous_time = current_time
+            self.total_rotations += 1
+            print(f'count: {self.total_rotations}\tdelta: {delta.total_seconds() * 1000}ms')
+            return (1, current_time)
 
         return (0, current_time)
--- worfometer.py
+++ worfometer.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 
-from odometer import Odometer
+from argparse import ArgumentParser
+from odometer import Odometer, MockOdometer
 from persistence import init_db, add_rotations
 import os
 import sqlite3
@@ -9,18 +10,24 @@ import time
 
 
 def main():
-    db_path = os.path.join(os.path.dirname(__file__), 'worfometer.sqlite')
+    parser = ArgumentParser(description="Monitor Worf's wheel and write stats to a database.")
+    parser.add_argument('--mock', action='store_true',
+                        help='Use the mock odometer instead of reading the hardware.')
+    parser.add_argument('--db', help='The path to the database file.')
+    args = parser.parse_args()
+
+    db_path = args.db or os.path.join(os.path.dirname(__file__), 'worfometer.sqlite')
 
     try:
         connection = sqlite3.connect(db_path)
         init_db(connection)
 
-        odometer = Odometer()
+        odometer = MockOdometer() if args.mock else Odometer()
 
         while True:
             rotations, timestamp = odometer.check_rotations()
             if rotations > 0:
-                add_rotations(connection, timestamp, rotations)
+                add_rotations(connection, rotations, timestamp)
 
             time.sleep(0.001)
     finally:
